91b6b38173e5aa90af6633dcbe79049fed7a9688
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-04-30 14:02:02 +0900

    Add files via upload

---
+++ user-service/api/schema/user.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    userid: str
+    name: str
+    email: str
+
+
+class UserLogin(BaseModel):
+    userid: str
+    passwd: str
+
+
+class UserCreate(UserBase):
+    passwd: str
+
+
+class User(UserBase):
+    mno: int
+    regdate: str
+
+    class Config:
+        from_attributes=True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
